107670c45041ac839eba8b3f2fb6d0058e24f698
Aliaksei Plashchanski <aleksey.ploschanskiy@gmail.com>
2018-03-20 15:44:46 +0300

    unused variables have been removed

--- src/version.h
+++ src/version.h
@@ -27,18 +27,7 @@ static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70810;
 //! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
-//! only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 32000;
-static const int NOBLKS_VERSION_END = 70909;
-
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
-
-//! "mempool" command, enhanced "getdata" behavior starts with this version
-static const int MEMPOOL_GD_VERSION = 60002;
-
-//! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70005;
-
 
 #endif // BITCOIN_VERSION_H
