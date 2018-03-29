19cce43d922b34f10e6480ca042c42001b711a73
nashanas <shagolo@gmail.com>
2018-03-29 11:24:41 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -94,7 +94,7 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0x91;
+        pchMessageStart[0] = 0x45;
         pchMessageStart[1] = 0xc5;
         pchMessageStart[2] = 0xfe;
         pchMessageStart[3] = 0xea;
